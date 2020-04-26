f53fcb1d4c5466fc1d2848f3c5d55f1be6569a45
Alexandre <alexfont@hotmail.fr>
2020-04-26 14:44:01 +0200

    super admin

--- src/Controller/SuperAdminController.php
+++ src/Controller/SuperAdminController.php
@@ -2,18 +2,23 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
-
+/**
+ * @Route("/{_locale}/superadmin")
+ */
 class SuperAdminController extends AbstractController
 {
     /**
-     * @Route("/super/admin", name="super_admin")
+     * @Route("/", name="super_admin")
      */
     public function index()
-    {
+    { $em = $this->getDoctrine()->getManager();
+        $users = $em->getRepository(User::class)->findAll(array(), array('id'=>'DESC'));
+
         return $this->render('super_admin/index.html.twig', [
-            'controller_name' => 'SuperAdminController',
+            'users' => $users,
         ]);
     }
 }
